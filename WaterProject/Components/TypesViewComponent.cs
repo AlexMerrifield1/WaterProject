ee98da1fe95345d3da6eaa63319c1e2ad03731d4
Alex Merrifield <alex.merrifield13@gmail.com>
2023-03-07 16:32:27 -0800

    Add project files.

---
+++ WaterProject/Components/TypesViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaterProject.Models;
+
+namespace WaterProject.Components
+{
+    public class TypesViewComponent : ViewComponent
+    {
+        private IWaterProjectRepository repo { get; set; }
+
+        public TypesViewComponent (IWaterProjectRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //This goes up into the entries and searches for project type, even if there isn't something there.
+            //Question mark allows it to be null
+            ViewBag.SelectedType = RouteData?.Values["projectType"];
+
+            var types = repo.Projects
+                .Select(x => x.ProjectType)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return View(types);
+        }
+    }
+}
